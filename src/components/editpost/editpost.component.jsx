36392c72f5a8f737f79c7b20adca4db9241323c1
hossam <hosamabdulmaged@gmail.com>
2022-04-13 03:45:16 +0200

    updated val from input edit-post

--- src/components/editpost/editpost.component.jsx
+++ src/components/editpost/editpost.component.jsx
@@ -1,14 +1,14 @@
 import Form from "react-bootstrap/Form";
 import {Button} from "react-bootstrap";
 import {useEffect, useState} from "react";
-import {UpdateSinglePost} from "../../redux/posts/post-action";
+import {Get_Single_post, UpdateSinglePost} from "../../redux/posts/post-action";
 import {connect} from "react-redux";
 import {toast} from 'react-toastify';
 import {useLocation, useNavigate} from 'react-router-dom';
 import {RapperHeaderComponent} from "../../page/homepage/homepage.styles";
 import Card from "react-bootstrap/Card";
 
-const EditPost = ({UpdateSinglePost, PostUpdated, postLoading}) => {
+const EditPost = ({UpdateSinglePost, PostUpdated, postLoading, postView}) => {
 
     const location = useLocation();
 
@@ -37,14 +37,17 @@ const EditPost = ({UpdateSinglePost, PostUpdated, postLoading}) => {
         toast.success(`Post Updated Successful`)
         // navigate('/')
     }
+    let result = location.pathname.slice(11);
 
     const Cancel = () => {
-        let result = location.pathname.slice(11);
         navigate(`/posts/${result}`)
     }
     useEffect(() => {
-
-    }, [postLoading])
+        Get_Single_post(result);
+        return () => {
+            setPost(postView)
+        }
+    }, [Get_Single_post, result, postLoading])
     return (
         <div className={'container'}>
             <div className={'row'}>
@@ -98,8 +101,10 @@ const EditPost = ({UpdateSinglePost, PostUpdated, postLoading}) => {
 const mapStataToProps = state => ({
     PostUpdated: state.posts.singlePost,
     postLoading: state.posts.flag,
+    postView: state.posts.singlePost,
 })
 const mapDispatchToProps = dispatch => ({
-    UpdateSinglePost: (post, headers) => dispatch(UpdateSinglePost(post, headers))
+    UpdateSinglePost: (post, headers) => dispatch(UpdateSinglePost(post, headers)),
+    Get_Single_post: (result) => dispatch(Get_Single_post(result))
 })
 export default connect(mapStataToProps, mapDispatchToProps)(EditPost);
